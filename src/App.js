173c20548ca3dba4c715dcae6e484dc10dce8d61
20160450 <olympiodoros@naver.com>
2021-01-16 05:02:45 +0900

    init test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>박건웅의 포트폴리오</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>initialize</div>;
 }
 
 export default App;
